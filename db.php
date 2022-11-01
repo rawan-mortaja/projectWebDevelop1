3b39f631f3725af4e7998e0aabfc0dcd75dc0cef
Rawan Mortaja <33564662+rawan-mortja@users.noreply.github.com>
2022-11-01 10:36:18 +0200

    Update db.php

--- db.php
+++ db.php
@@ -5,22 +5,22 @@
     $pass = '';
     $db = 'project';
   $con = mysqli_connect($host , $user , $pass , $db);
-    $dsn = "mysql:host=$host;dbname=$db;user=$user;pass=$pass";
+//     $dsn = "mysql:host=$host;dbname=$db;user=$user;pass=$pass";
 
 
-    try{
-        $dbConn = new PDO($dsn , $user , $pass);
-        $dbConn->setAttribute(PDO::ATTR_ERRMODE, PDO::ERRMODE_EXCEPTION);
-       // echo "Connection succecssful";
-        $sql = "SELECT * FROM users";
-        $users = $dbConn->query($sql);
-        foreach($users AS $user){
-        //      echo "<li>".$user["name"]."</li>";
-        }
+//     try{
+//         $dbConn = new PDO($dsn , $user , $pass);
+//         $dbConn->setAttribute(PDO::ATTR_ERRMODE, PDO::ERRMODE_EXCEPTION);
+//        // echo "Connection succecssful";
+//         $sql = "SELECT * FROM users";
+//         $users = $dbConn->query($sql);
+//         foreach($users AS $user){
+//         //      echo "<li>".$user["name"]."</li>";
+//         }
       
 
-    }catch(PDOExecption $error){
-        echo $error->getCode();
-    }
+//     }catch(PDOExecption $error){
+//         echo $error->getCode();
+//     }
 
 ?>
